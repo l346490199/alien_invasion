4e0e55678d2ee71ee1a310a57ecdf924b9d9e6fe
Autumn.L <q346490199@outlook.com>
2019-08-18 11:43:50 +0800

    子弹可以发送了  还会子弹删除 q键结束                                   Signed-off-by: Autumn.L <q346490199@outlook.com>

--- bullet.py
+++ bullet.py
@@ -5,7 +5,7 @@
 # @email lq@aqiu.info
 # @description 子弹类
 # @created 2019-08-18T10:00:13.983Z+08:00
-# @last-modified 2019-08-18T10:24:15.567Z+08:00
+# @last-modified 2019-08-18T11:33:26.246Z+08:00
 #
 
 import pygame
@@ -22,7 +22,7 @@ class Bullet(Sprite):
         # 在（0，0） 处创建一个表示子弹的矩形，在设置正确的位置
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
                                 ai_settings.bullet_height)
-        self.rect.center = ship.rect.centerx
+        self.rect.centerx = ship.rect.centerx
         # top顶部
         self.rect.top = ship.rect.top
         # 存储小数表示子弹的位置
@@ -31,7 +31,7 @@ class Bullet(Sprite):
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
 
-    def Update(self):
+    def update(self):
         ''' 向上移动子弹'''
         # 更新表示子弹位置的小数值
         self.y -= self.speed_factor
--- game_functions.py
+++ game_functions.py
@@ -5,13 +5,14 @@
 # @email lq@aqiu.info
 # @description 补充invasion
 # @created 2019-08-16T11:47:13.765Z+08:00
-# @last-modified 2019-08-18T10:59:59.525Z+08:00
+# @last-modified 2019-08-18T12:13:26.501Z+08:00
 #
 
 import sys
 
 import pygame
 from bullet import Bullet
+
 
 def check_events(ai_settings, screen, ship, bullets):
     '''响应按键和鼠标事件'''
@@ -22,10 +23,11 @@ def check_events(ai_settings, screen, ship, bullets):
             sys.exit(0)
         # 判断按键按下
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         # 判断 按键松开
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+
 
 def check_keyup_events(event, ship):
     ''' 当按键松开时'''
@@ -37,8 +39,11 @@ def check_keyup_events(event, ship):
         ship.moving_up = False
     if event.key == pygame.K_DOWN:
         ship.moving_down = False
+    if event.key == pygame.K_q:
+        sys.exit(0)
 
-def check_keydown_events(event, ship):
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     '''当按键按下时'''
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -48,12 +53,19 @@ def check_keydown_events(event, ship):
         ship.moving_up = True
     if event.key == pygame.K_DOWN:
         ship.moving_down = True
-    
+    if event.key == pygame.K_SPACE:
+        #按下空格生成一个子弹，并将其加入到编组bullets中
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
 
 def update_screen(ai_settings, screen, ship, bullets):
     ''' 更新屏幕上的图像，并切换到新屏幕'''
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     #放置飞船
     ship.blitme()
     #让最近绘制的屏幕可见
